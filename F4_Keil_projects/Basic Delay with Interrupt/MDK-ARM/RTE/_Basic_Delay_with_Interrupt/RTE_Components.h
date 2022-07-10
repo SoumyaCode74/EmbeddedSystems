94db6ce3747638e7406933aa772722de0752de17
SoumyaCode74 <100553212+SoumyaCode74@users.noreply.github.com>
2022-07-11 02:23:17 +0530

    Basic Delay timer, uncontrolled and controlled

---
+++ F4_Keil_projects/Basic Delay with Interrupt/MDK-ARM/RTE/_Basic_Delay_with_Interrupt/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Basic Delay with Interrupt' 
+ * Target:  'Basic Delay with Interrupt' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
