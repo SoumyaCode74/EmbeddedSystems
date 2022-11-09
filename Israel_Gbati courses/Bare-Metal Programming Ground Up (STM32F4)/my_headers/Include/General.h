d4fcc0c3e76aa30debdad371be274df6d6fdfc5c
SoumyaCode74 <100553212+SoumyaCode74@users.noreply.github.com>
2022-11-09 21:52:41 +0530

    header files for adc and general parameters. Disclaimer: the adc.h is yet to be made more generalized

---
+++ Israel_Gbati courses/Bare-Metal Programming Ground Up (STM32F4)/my_headers/Include/General.h
@@ -0,0 +1,15 @@
+/*
+ * General.h
+ *
+ *  Created on: 06-Nov-2022
+ *      Author: soumy
+ */
+
+#ifndef GENERAL_H_
+#define GENERAL_H_
+
+#define DELAY_HIGH			500000UL
+#define DELAY_LOW  			200000UL
+#define SMALL_DELAY			1000U
+
+#endif /* GENERAL_H_ */
