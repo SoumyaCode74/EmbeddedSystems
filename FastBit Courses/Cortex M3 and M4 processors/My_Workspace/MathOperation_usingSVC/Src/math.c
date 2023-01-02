5d982969d581adb400c7c9021f3fc702d6ad0d72
SoumyaCode74 <100553212+SoumyaCode74@users.noreply.github.com>
2023-01-03 02:06:06 +0530

    Code files for executing math operations using Supervisor Call Exception handler

---
+++ FastBit Courses/Cortex M3 and M4 processors/My_Workspace/MathOperation_usingSVC/Src/math.c
@@ -0,0 +1,23 @@
+/*
+ * math.c
+ *
+ *  Created on: Jan 1, 2023
+ *      Author: soumy
+ */
+
+#include <stdint.h>
+
+extern int32_t addition(int32_t a, int32_t b){
+	return a + b;
+}
+extern int32_t subtraction(int32_t a, int32_t b){
+	return a - b;
+}
+extern int32_t multiplication(int32_t a, int32_t b){
+	return a * b;
+}
+extern float division(int32_t a, int32_t b){
+	/* Enable floating point unit */
+	return a / b;
+}
+
